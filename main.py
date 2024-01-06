5f97bdfa5f60def4de7d2357322300ed644f0e11
thaone31 <99576488+thaone31@users.noreply.github.com>
2024-01-06 22:49:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-
-!mkdir -p vncorenlp/models/wordsegmenter
-!wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/VnCoreNLP-1.1.1.jar
-!wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/models/wordsegmenter/vi-vocab
-!wget https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/models/wordsegmenter/wordsegmenter.rdr
-!mv VnCoreNLP-1.1.1.jar vncorenlp/
-!mv vi-vocab vncorenlp/models/wordsegmenter/
-!mv wordsegmenter.rdr vncorenlp/models/wordsegmenter/
-
 import os
 import torch
 import pickle
